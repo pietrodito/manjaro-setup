3640cd1275744c86fbd2de15355597b2a6019038
pietrodito <pietrodito@gmail.com>
2020-02-24 10:26:16 +0100

    RStudio setup.

--- installers/R-setup.sh
+++ installers/R-setup.sh
@@ -1,1 +1,14 @@
 sudo pacman --sync --refresh --sysupgrade --noconfirm --needed r gcc-fortran tk
+
+# RSTUDIO :
+AUR_DIR="$HOME/Comp/AUR/rstudio-desktop"
+sudo rm --recursive --force "$AUR_DIR"
+git clone https://aur.archlinux.org/rstudio-desktop.git "$AUR_DIR"
+
+cd "$AUR_DIR"
+makepkg --syncdeps --install --rmdeps --clean --noconfirm --needed
+
+#     Qt hack NEEDED !
+#TODO Check https://aur.archlinux.org/packages/rstudio-desktop/
+#     To know when this Qt hack is not needed anymore :
+ln -s /usr/share/qt/resources ~/.QtWebEngineProcess
