e872c9f3794f064e3422f81fd6360eb878379a15
pietrodito <pietrodito@gmail.com>
2020-03-15 10:45:06 +0100

    Prompt changed...

--- installers/R-setup.sh
+++ installers/R-setup.sh
@@ -1,6 +1,6 @@
 sudo pacman --sync --refresh --sysupgrade --noconfirm --needed r gcc-fortran tk
 
-# RSTUDIO :
+# RSTUDIO:
 AUR_DIR="$HOME/Comp/AUR/rstudio-desktop"
 sudo rm --recursive --force "$AUR_DIR"
 git clone https://aur.archlinux.org/rstudio-desktop.git "$AUR_DIR"
@@ -12,3 +12,6 @@ makepkg --syncdeps --install --rmdeps --clean --noconfirm --needed
 #TODO Check https://aur.archlinux.org/packages/rstudio-desktop/
 #     To know when this Qt hack is not needed anymore :
 ln -s /usr/share/qt/resources ~/.QtWebEngineProcess
+
+# PANDOC:
+sudo pacman -Syu pandoc
