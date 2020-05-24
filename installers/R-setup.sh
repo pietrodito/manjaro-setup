e1e64d6b81487c438cb48dc5b12033b3b0a5a467
pietrodito <pietrodito@gmail.com>
2020-05-24 07:41:46 +0200

    Minimal OK go for emacs/R/autokey/zotero now...

--- installers/R-setup.sh
+++ installers/R-setup.sh
@@ -1,17 +1,17 @@
 sudo pacman --sync --refresh --sysupgrade --noconfirm --needed r gcc-fortran tk
 
 # RSTUDIO:
-AUR_DIR="$HOME/Comp/AUR/rstudio-desktop"
-sudo rm --recursive --force "$AUR_DIR"
-git clone https://aur.archlinux.org/rstudio-desktop.git "$AUR_DIR"
+# AUR_DIR="$HOME/Comp/AUR/rstudio-desktop"
+# sudo rm --recursive --force "$AUR_DIR"
+# git clone https://aur.archlinux.org/rstudio-desktop.git "$AUR_DIR"
 
-cd "$AUR_DIR"
-makepkg --syncdeps --install --rmdeps --clean --noconfirm --needed
+# cd "$AUR_DIR"
+# makepkg --syncdeps --install --rmdeps --clean --noconfirm --needed
 
 #     Qt hack NEEDED !
 #TODO Check https://aur.archlinux.org/packages/rstudio-desktop/
 #     To know when this Qt hack is not needed anymore :
-ln -s /usr/share/qt/resources ~/.QtWebEngineProcess
+# ln -s /usr/share/qt/resources ~/.QtWebEngineProcess
 
 # PANDOC:
-sudo pacman -Syu pandoc
+sudo pacman --sync --refresh --sysupgrade --noconfirm --needed pandoc
--- installers/command-line-tools.sh
+++ installers/command-line-tools.sh
@@ -3,7 +3,8 @@ git clone https://aur.archlinux.org/peco.git ~/Comp/AUR/peco
 
 cd ~/Comp/AUR/peco
 makepkg --syncdeps --install --rmdeps --clean --noconfirm --needed
+mkdir --parents $HOME/.config/peco
+ln --symbolic --force $REPO_DIR/helpers/peco-config.json $HOME/.config/peco/config.json
 
 sudo pacman --sync --refresh --sysupgrade --noconfirm --needed fasd
 
-#TODO add eval "$(fasd --init auto)" inside zsh
