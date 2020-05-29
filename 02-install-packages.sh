6e2092aa5b6ce5162309ea19eb71c7326bd16d43
pietrodito <pietrodito@gmail.com>
2020-05-29 17:15:33 +0200

    yay is now installed form arch repo

---
+++ 02-install-packages.sh
@@ -0,0 +1,21 @@
+sudo pacman --sync --refresh --sysupgrade --noconfirm
+
+sudo pacman --sync --noconfirm --needed tree
+sudo pacman --sync --noconfirm --needed xcape
+sudo pacman --sync --noconfirm --needed fasd
+sudo pacman --sync --noconfirm --needed terminator
+sudo pacman --sync --noconfirm --needed tmux
+
+sudo pacman --sync --noconfirm --needed emacs
+
+sudo pacman --sync --noconfirm --needed nextcloud-client
+sudo pacman --sync --noconfirm --needed libreoffice-fresh
+sudo pacman --sync --noconfirm --needed r gcc-fortran tk
+sudo pacman --sync --noconfirm --needed inkscape
+
+sudo pacman --sync --noconfirm --needed base-devel
+sudo pacman --sync --noconfirm --needed yay
+yay         --sync --noconfirm --needed peco-bin
+yay         --sync --noconfirm --needed autokey
+yay         --sync --noconfirm --needed zotero
+
--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 ./00-git-and-vim-setup.sh
 ./01-oh-my-zsh-setup.sh
-./02-yay-setup.sh
-./03-install-packages.sh
-./04-config-symlins.sh
+./02-install-packages.sh
+./03-config-symlins.sh
