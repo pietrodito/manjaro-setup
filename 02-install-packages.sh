d7b320779e2a083e7aeeab5fcf9db2105c0c19d0
pietrodito <pietrodito@gmail.com>
2020-09-25 16:25:03 +0200

    Fix spotify GPG keys

--- 02-install-packages.sh
+++ 02-install-packages.sh
@@ -36,6 +36,7 @@ Packages=(
 )
 
 ## FIXME Spotify hack
+curl -sS https://download.spotify.com/debian/pubkey.gpg | gpg --import -
 gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
 
 
